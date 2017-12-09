418a2b98d42ecd28cf807b4362526c0047534e12
jluongh <luong.jenniferh@gmail.com>
2017-12-09 14:17:48 -0800

    Updated Itinerary files

--- EnTest/Itinerary_ViewController.swift
+++ EnTest/Itinerary_ViewController.swift
@@ -16,7 +16,7 @@ class Itinerary_ViewController: UIViewController, UITableViewDelegate, UITableVi
     let timeList = ["2:00", "3:30", "5:00", "6:00"]
     let ampmList = ["PM", "PM", "PM", "PM"]
     let eventList = ["Flight", "Hotel Check-In", "Day Club", "More club"]
-    let descriptionList = ["test", "test2", "test3", "test4"]
+    let descriptionList = ["testfdgsdfgsdfgfds \n fdgsd", "test2", "test3", "test4"]
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return eventList.count
@@ -25,10 +25,10 @@ class Itinerary_ViewController: UIViewController, UITableViewDelegate, UITableVi
     public func tableView (_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItineraryCell", for: indexPath) as! ItineraryTableViewCell
         
-//        cell.StartTime.text = timeList[indexPath.row]
-//        cell.ampm.text = ampmList[indexPath.row]
+        cell.StartTime.text = timeList[indexPath.row]
+        cell.ampm.text = ampmList[indexPath.row]
         cell.Event.text = eventList[indexPath.row]
-//        cell.Descript.text = descriptionList[indexPath.row]
+        cell.Descript.text = descriptionList[indexPath.row]
         
         return cell
     }
@@ -38,6 +38,8 @@ class Itinerary_ViewController: UIViewController, UITableViewDelegate, UITableVi
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.ItineraryTableView.delegate = self
+        self.ItineraryTableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
