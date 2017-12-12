1c5eb57fc3dd6180e735b435a42cb09a2cdce3b0
Alex Berthon <Alexjberthon@hotmail.com>
2017-12-12 15:02:34 -0800

    Create account, database connection

--- EnTest/CreateAccount_ViewController.swift
+++ EnTest/CreateAccount_ViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
+import Alamofire
+import UIKit
+
 class CreateAccount_ViewController: UIViewController {
     
-    let urlPath = URL(string:"https://en-route.000webhostapp.com/user.php")
+    let URL_USER_REGISTER = URL(string:"https://en-route.000webhostapp.com/register.php")
     
     @IBOutlet weak var textFieldFirstName: UITextField!
     @IBOutlet weak var textFieldLastName: UITextField!
@@ -20,62 +23,34 @@ class CreateAccount_ViewController: UIViewController {
     @IBOutlet weak var textFieldPassword: UITextField!
 
     @IBAction func createAccount(sender: UIButton) {
+        
         //creating NSMutableURLRequest
-        let request = NSMutableURLRequest(url: urlPath!)
-        
-        //setting the method to post
-        request.httpMethod = "POST"
-        
-        //getting values from text fields
-        let firstName = textFieldFirstName.text
-        let lastName = textFieldFirstName.text
-        let email = textFieldEmail.text
-        let phone = textFieldPhone.text
-        let username = textFieldUsername.text
-        let password = textFieldPassword.text
-
-        //creating the post parameter by concatenating the keys and values from text field
-        let postParameters = "FirstName="+firstName!+"LastName="+lastName!+"Email"+email!+"Phone"+phone!+"Username"+username!+"Password"+password!;
-        
-        //adding the parameters to request body
-        request.httpBody = postParameters.data(using:String.Encoding.utf8)
-        
-        
-        //creating a task to send the post request
-        let task = URLSession.shared.dataTask(with: request as URLRequest){
-            data, response, error in
-            
-            if error != nil{
-                print("error is \(error)")
-                return;
+        let parameters: Parameters=[
+            "Username":textFieldUsername.text!,
+            "Password":textFieldPassword!,
+            "FirstName":textFieldFirstName!,
+            "LastName":textFieldLastName!,
+            "Email":textFieldEmail!,
+            "Phone":textFieldPhone!
+            ]
+         
+         //Sending http post request
+         Alamofire.request(URL_USER_REGISTER, method: .post, parameters: parameters).responseJSON
+         {
+            response in
+            //printing response
+            print(response)
+         
+             //getting the json value from the server
+                if let result = response.result.value {
+         
+                    //converting it as NSDictionary
+                    let jsonData = result as! NSDictionary
+                    
+                    //displaying the message in label
+                    self.labelMessage.text = jsonData.value(forKey: "message") as! String?
             }
-            
-            //parsing the response
-            do {
-                //converting resonse to NSDictionary
-                let myJSON =  try JSONSerialization.jsonObject(with:data!,options:.mutableContainers)as? NSDictionary
-                
-                //parsing the json
-                if let parseJSON = myJSON {
-                    
-                    //creating a string
-                    var msg : String!
-                    
-                    //getting the json response
-                    msg = parseJSON["message"] as! String?
-                    
-                    //printing the response
-                    print(msg)
-                    
-                }
-            } catch {
-                print(error)
-            }
-            
-        }
-        //executing the task
-        task.resume()
-        
+         }
     }
     
     override func viewDidLoad() {
@@ -88,16 +63,4 @@ class CreateAccount_ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
