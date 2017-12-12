484aa82332629bad1f2f1b170f8ecacbdd61a52e
Leofric <alexjberthon@hotmail.com>
2017-12-12 14:15:34 -0800

    Create constants.php

---
+++ docs/constants.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_USERNAME', 'cheatcodes');
+define('DB_PASSWORD', 'password1');
+define('DB_HOST', 'db4free.net:3307');
+define('DB_NAME', 'enroute');
+ 
+define('USER_CREATED', 0);
+define('USER_ALREADY_EXIST', 1);
+define('USER_NOT_CREATED', 2);
